d9e907d67f5ecdcd47b31c5689fecfc7df7c2000
tobiajras <tobiajras@hotmail.com>
2024-05-04 12:46:17 -0300

    technologies fixed in mobile

--- src/app/technology/TechnologySection.jsx
+++ src/app/technology/TechnologySection.jsx
@@ -8,7 +8,7 @@ const TechnologySection = () => {
       id='technologySection'
       className='flex justify-center bg-color-primary w-full py-20 sm:py-16 md:py-32 -mt-10'
     >
-      <div className='max-w-6xl flex items-center w-full mx-6 sm:mx-8 md:mx-10'>
+      <div className='max-w-6xl flex flex-col gap-6 sm:gap-0 sm:flex-row items-center w-full mx-6 sm:mx-8 md:mx-10'>
         <article className='flex flex-col justify-center w-full'>
           <h5 className='sm:text-nowrap text-color-title text-lg sm:text-2xl md:text-3xl mb-2 sm:mb-3 md:mb-5'>
             We use cutting edge technology
@@ -22,7 +22,7 @@ const TechnologySection = () => {
             faucibus dolor auctor.
           </p>
         </article>
-        <article className='grid grid-cols-2 gap-x-3 gap-y-3 pl-3 sm:gap-x-10 md:gap-x-10 lg:gap-x-24 sm:gap-y-10 sm:p-10 md:p-12'>
+        <article className='grid grid-cols-6 sm:grid-cols-2 gap-x-3 gap-y-3 pl-3 sm:gap-x-10 md:gap-x-10 lg:gap-x-24 sm:gap-y-10 sm:p-10 md:p-12'>
           {technologies.map((icon, index) => (
             <div className='h-10 sm:h-16 md:h-24' key={index}>
               <Image
